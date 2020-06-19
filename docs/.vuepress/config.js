da800301c810087c350a9ca60cdd46d04340caed
shinyaueda <info@sndbox.jp>
2020-06-19 21:24:46 +0900

    w

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -15,22 +15,47 @@ module.exports = {
     }
   },
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }],
     ['meta',{ name:"keywords", content:"クラウド,日報,nipo,業務報告書"}],
     ['meta',{ name:"og:title", content:"クラウド日報Nipo"}],
     ['meta',{ name:"og:description", content:"スマホやPCから日報作成"}],
     ['meta',{ name:"og:type", content:"website"}],
     ['meta',{ name:"og:url", content:"https://nipo.sndbox.jp"}],
     ['meta',{ name:"og:image", content: '/logo.png' }],
-    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    ['meta', { name: 'viewport', content: 'user-scalable=no,initial-scale=1,maximum-scale=1,minimum-scale=1,width=device-width' }],
     ['link', { rel: 'manifest', href: '/manifest.json' }],
     ['meta', { name: 'theme-color', content: '#027be3' }],
+
+    ['link', { rel: 'icon', type: "image/png", sizes: "128x128", href: "/icons/favicon-128x128.png" }],
+    ['link', { rel: 'icon', type: "image/png", sizes: "96x96", href: "/icons/favicon-96x96.png" }],
+    ['link', { rel: 'icon', type: "image/png", sizes: "32x32", href: "/icons/favicon-32x32.png" }],
+    ['link', { rel: 'icon', type: "image/png", sizes: "16x16", href: "/icons/favicon-16x16.png" }],
+    ['link', { rel: 'icon', type: "image/ico", href: "/icons/favicon.ico" }],
+
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-icon-120x120.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: "152x152", href: '/icons/apple-icon-152x152.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: "167x167", href: '/icons/apple-icon-167x167.png' }],
+    ['link', { rel: 'apple-touch-icon', sizes: "180x180", href: '/icons/apple-icon-180x180.png' }],
+
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-828x1792.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3)', href: "/icons/apple-launch-1125x2436.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3)', href: "/icons/apple-launch-1242x2688.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-750x1334.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3)', href: "/icons/apple-launch-1242x2208.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-640x1136.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-1536x2048.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-1668x2224.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-1668x2388.png" }],
+    ['link', { rel: 'apple-touch-startup-image', media: '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2)', href: "/icons/apple-launch-2048x2732.png" }],
+
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'default' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'クラウド日報Nipo公式' }],
+
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#027be3' }],
+
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/ms-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }],
+    ['meta', { name: 'msapplication-tap-highlight', content: 'no' }],
   ],
   plugins: [
     ['@vuepress/pwa', {
@@ -102,6 +127,7 @@ module.exports = {
           children: [
             ['/manual/res/res', '日報を受け取る'],
             ['/manual/res/reaction', '承認・棄却・既読する'],
+            ['/manual/res/tags', '日報にタグを付ける'],
             ['/manual/res/comment', '日報にコメントをする'],
             ['/manual/res/search', '日報を検索する'],
 
@@ -178,7 +204,9 @@ module.exports = {
             ['/manual/template/pict', '【パーツ】写真'],
             ['/manual/template/sign', '【パーツ】署名'],
             ['/manual/template/canvas', '【パーツ】手書き'],
+            ['/manual/template/separator', '【パーツ】区切り'],
             ['/manual/template/table', '【パーツ】表'],
+            ['/manual/template/guidearea', 'ガイドエリアについて'],
           ]
         },
         {
